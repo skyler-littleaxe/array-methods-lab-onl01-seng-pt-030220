1b68d91e2b9786bc7513d6d018ec2aa8a7c99f0e
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 02:56:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,7 +2,7 @@ def using_include(array, "wow")
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?("wow")
 end
-  using_include(array, "Tardis")
+def usinginclude(array, "Tardis")
   array.include?("Tardis")
 end
 
