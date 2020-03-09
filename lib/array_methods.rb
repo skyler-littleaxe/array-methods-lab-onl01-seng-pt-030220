cf207aa6f659c5c488e20d5ac73f68c05e575951
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 02:14:27 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element1)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("element1", "boop")
+  array.include?("element1")
 end
 
 def using_sort(array)
