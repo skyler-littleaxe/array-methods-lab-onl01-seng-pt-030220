d846e4536b8b64c24354887ce6a90865cb5374f0
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 03:00:06 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def using_include(array, "wow")
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.include?("wow")
-  def using_include(array, "Tardis")
 end
 
 def using_sort(array)
