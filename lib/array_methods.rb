c6bfb385ab06f9028858da8d956a86276173f10d
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-09 02:06:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,6 @@
 def using_include(array, element1)
   array = ["dance", "element1", "warp"]
-  array.include?("element1")
-  array.include?("boop")
+  array.include?("element1", "boop")
 end
 
 def using_sort(array)
